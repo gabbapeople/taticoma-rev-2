953d1af36f655b6fcd796f15b8a5af58b2b9ef41
Max Danilin <gabbapeople@mail.ru>
2020-02-23 19:14:34 +0300

    23.02.2020-w

--- GIMBAL/o323bgc-release-v096-v20160319/o323BGCTool_Run.bat
+++ GIMBAL/o323bgc-release-v096-v20160319/o323BGCTool_Run.bat
@@ -1,4 +1,4 @@
-"bin\ST\STMFlashLoader\STMFlashLoaderOlliW.exe" -c --pn 3 --br 115200 -ow -d --fn "o323BgcNtFirmwareFiles\o323bgc_ntimu_v019_module_v10_f103t8.hex" --ep --v -r --a 8000000
+"bin\ST\STMFlashLoader\STMFlashLoaderOlliW.exe" -c --pn 14 --br 115200 -ow -e --all -d --fn "o323BgcFirmwareFiles\o323bgc_v096_storm32bgc_v130_f103rc.hex" --v -r --a 8000000
 
 @echo.
 @pause
